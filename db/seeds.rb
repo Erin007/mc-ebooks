ba99020829a2fba1ec17275efc2a0408f4fd9e20
Erin Bond <>
2021-12-20 13:01:47 -0500

    seed books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'books.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  b = Book.new
+  b.title = row['title']
+  b.author = row['author']
+  b.isbn = row['isbn']
+  b.save
+  puts "#{b.title} added to the library"
+end
+
+puts "There are now #{Book.count} row in the books table."
