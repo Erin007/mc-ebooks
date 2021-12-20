0fb076384aa5d0b0ff989041af20be919a40c485
Erin Bond <>
2021-12-20 12:01:17 -0500

    add annotate gem for model files

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  isbn       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_books_on_author  (author)
+#  index_books_on_isbn    (isbn)
+#  index_books_on_title   (title)
+#
 class Book < ApplicationRecord
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  isbn       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_books_on_author  (author)
+#  index_books_on_isbn    (isbn)
+#  index_books_on_title   (title)
+#
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
